0214d47acd5c8c0fb4874f328696b5b94a71bb27
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-13 14:12:14 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="w-full h-screen flex flex-col background relative overflow-x-hidden items-center">
       <h1 className="bg-white absolute rounded-lg w-11/12 text-center mt-[40px]
-        px-10 py-2 text-4xl font-bold mx-auto">
+        px-10 py-2 text-4xl font-bold">
       Random Gifs</h1>
       <div className="flex flex-col">
         <Random/>
