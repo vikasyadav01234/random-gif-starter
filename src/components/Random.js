a563bfab388f1db29ec3cc150abe7a733e440e59
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-13 19:30:08 +0530

    main

--- src/components/Random.js
+++ src/components/Random.js
@@ -3,11 +3,11 @@ import React, { useState } from "react";
 const Random = () => {
   const [gif, setGif] = useState('');
   function clickHandler() {
-    
+
   }
 
   return (
-    <div>
+    <div className="w-full h-[450px] bg-green-500">
       <h1>Random Gif</h1>
       <img src={gif} width="450"/>
       <button onClick={clickHandler}>
