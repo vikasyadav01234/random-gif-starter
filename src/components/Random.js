fcfe4e81f05c39cec71ffe5b052c4c8b66ceb79f
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-13 19:25:11 +0530

    main

--- src/components/Random.js
+++ src/components/Random.js
@@ -6,7 +6,10 @@ const Random = () => {
   return (
     <div>
       <h1>Random Gif</h1>
-      <img src={gif}/>
+      <img src={gif} width="450"/>
+      <button>
+        Generate
+      </button>
     </div>
   )
 }
