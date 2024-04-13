dc62e1ab9e41467fb901a3c888d115061ddeb367
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-13 19:48:42 +0530

    main

--- src/components/Random.js
+++ src/components/Random.js
@@ -8,7 +8,7 @@ const Random = () => {
 
   return (
     <div className="w-1/2 h-[450px] bg-green-500 rounded-lg border border-black
-    flex flex-col items-center gap-y-5">
+    flex flex-col items-center gap-y-5 mt-[15px]">
       <h1>Random Gif</h1>
       <img src={gif} width="450"/>
       <button onClick={clickHandler}>
