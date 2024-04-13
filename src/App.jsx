c9f9211c8f2c44dea5d0a0fb7d3c4193dd75a890
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-13 13:58:59 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 export default function App() {
   return (
-    <div>
+    <div >
       <h1 className="bg-white rounded-sm">Random Gifs</h1>
-      <div>
-
+      <div className="flex flex-col">
+        <Random/>
+        <Tag/>
       </div>
     </div>
   )
