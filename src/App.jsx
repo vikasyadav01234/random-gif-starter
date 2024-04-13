93def4b027669ccfa2ef5ed6ae92efa9289bd0dd
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-13 14:11:43 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Tag from "./components/Tag"
 
 export default function App() {
   return (
-    <div className="w-full h-screen flex flex-col background relative overflow-x-hidden item-center">
+    <div className="w-full h-screen flex flex-col background relative overflow-x-hidden items-center">
       <h1 className="bg-white absolute rounded-lg w-11/12 text-center mt-[40px]
         px-10 py-2 text-4xl font-bold mx-auto">
       Random Gifs</h1>
