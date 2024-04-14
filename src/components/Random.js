fb052a7b345ba51206cc0a47da60aa89d2e567f4
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-14 14:53:44 +0530

    main

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Spinner from "./Spinner";
 
 
 const API_KEY = process.env.REACT_APP_GIPHY_API_KEY
@@ -26,7 +27,11 @@ const Random = () => {
     <div className="w-1/2 h-[450px] bg-green-500 rounded-lg border border-black
     flex flex-col items-center gap-y-5 mt-[15px]">
       <h1 className="mt-[15px] text-xl underline uppercase font-bold">A Random Gif</h1>
-      <img src={gif} width="450"/>
+
+      {
+        loading ? (<Spinner/>):()
+      }
+      
       <button onClick={clickHandler}
       className="w-10/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px]"
       >
