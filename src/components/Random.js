7dd6fc3d1ec67f9f38a02fe612d3e1c39e1f8c36
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-14 14:37:39 +0530

    main

--- src/components/Random.js
+++ src/components/Random.js
@@ -28,7 +28,7 @@ const Random = () => {
       <h1 className="mt-[15px] text-xl underline uppercase font-bold">A Random Gif</h1>
       <img src={gif} width="450"/>
       <button onClick={clickHandler}
-      className="w-10/12 bg-yellow-500 text-lg py-2 rounded-lg"
+      className="w-10/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px]"
       >
         Generate
       </button>
