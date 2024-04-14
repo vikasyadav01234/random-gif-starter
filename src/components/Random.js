eb616b1d94a793cdc674782503c21fc13a14c25c
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-14 15:18:21 +0530

    main

--- src/components/Random.js
+++ src/components/Random.js
@@ -9,13 +9,15 @@ const Random = () => {
   const [gif, setGif] = useState('');
   const [loading, setloading] = useState('false')
   async function fetchData(){
-    
+
+    setloading(true);
     const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
     const {data} = await axios.get(url);
 
     const imageSource = data.data.images.downsized_large.url;
   
     setGif(imageSource);
+    setloading(false);
   }
 
   useEffect( () => {
