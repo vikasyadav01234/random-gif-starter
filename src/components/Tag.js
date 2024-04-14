bcdf851c05367964e8f7798cfb920ec3af674792
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-14 19:23:49 +0530

    main

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -27,6 +27,10 @@ const Tag = () => {
     fetchData();
   }
 
+  function changeHandler(){
+    
+  }
+
   return (
     <div className="w-1/2 h-[450px] bg-blue-500 rounded-lg border border-black
     flex flex-col items-center gap-y-5 mt-[15px]">
@@ -38,6 +42,7 @@ const Tag = () => {
 
       <input
         className="w-10/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[3px] text-center"
+        onChange={changeHandler}
       />
       
       <button onClick={clickHandler}
